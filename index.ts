10b416236df78d21ff0d3895e4bbbbfabb7df9d4
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-17 01:19:55 +0530

    configure server

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import contactInfo from "./src/routes/contactInfo";
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Directing to the routes
+app.use('/contact', contactInfo)
+
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}...`)
+})
+
+
